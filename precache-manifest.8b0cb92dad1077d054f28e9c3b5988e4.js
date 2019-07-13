351b08e45bc7344139a707661bc5d8a43c615d71
John Cambi <jcambi@myseneca.ca>
2019-07-13 15:42:31 -0400

    Updates

---
+++ precache-manifest.8b0cb92dad1077d054f28e9c3b5988e4.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242a9c53e3ce6ba610f403d7a0f24afd",
+    "url": "/react-crypto-monitor/index.html"
+  },
+  {
+    "revision": "5cd4910d359780b8560b",
+    "url": "/react-crypto-monitor/static/css/main.cf5c0f1c.chunk.css"
+  },
+  {
+    "revision": "76679284482e125bc311",
+    "url": "/react-crypto-monitor/static/js/2.824d84ea.chunk.js"
+  },
+  {
+    "revision": "5cd4910d359780b8560b",
+    "url": "/react-crypto-monitor/static/js/main.c8912735.chunk.js"
+  },
+  {
+    "revision": "42c5d68e417523e70a9a",
+    "url": "/react-crypto-monitor/static/js/runtime~main.e4b90743.js"
+  },
+  {
+    "revision": "bf016ce679ce1610a8cc75bf48fc283c",
+    "url": "/react-crypto-monitor/static/media/Binance Coin.bf016ce6.svg"
+  },
+  {
+    "revision": "8a7e1fe0f5ac5170602f22d64ac54ec2",
+    "url": "/react-crypto-monitor/static/media/Bitcoin Cash.8a7e1fe0.svg"
+  },
+  {
+    "revision": "142ca3f97cf7a27f5b5b5f1357586c12",
+    "url": "/react-crypto-monitor/static/media/Bitcoin.142ca3f9.svg"
+  },
+  {
+    "revision": "180089e5c2bdeb5fd7ab6725d2515fe7",
+    "url": "/react-crypto-monitor/static/media/BitcoinSV.180089e5.svg"
+  },
+  {
+    "revision": "f9e315c5d56c345f7b187350c97ac2fb",
+    "url": "/react-crypto-monitor/static/media/Cardano.f9e315c5.svg"
+  },
+  {
+    "revision": "ba4a9c9df524c919ca65f3d4c0cb4e59",
+    "url": "/react-crypto-monitor/static/media/Cosmos.ba4a9c9d.svg"
+  },
+  {
+    "revision": "d7f5061ea71b64485c3411673c22b2c5",
+    "url": "/react-crypto-monitor/static/media/Dash.d7f5061e.svg"
+  },
+  {
+    "revision": "ec2e46389238763a039b36eaae1fbe25",
+    "url": "/react-crypto-monitor/static/media/EOS.ec2e4638.svg"
+  },
+  {
+    "revision": "519ec9359fd2e9b01d20d47b2f4ece43",
+    "url": "/react-crypto-monitor/static/media/Ethereum Classic.519ec935.svg"
+  },
+  {
+    "revision": "dd303e8905e48929f968e6effbcfc4dd",
+    "url": "/react-crypto-monitor/static/media/Ethereum.dd303e89.svg"
+  },
+  {
+    "revision": "c3d32599d7d889c916b3421720091bc7",
+    "url": "/react-crypto-monitor/static/media/IOTA.c3d32599.svg"
+  },
+  {
+    "revision": "97449994cb96c9925c6ad5e94e253736",
+    "url": "/react-crypto-monitor/static/media/Litecoin.97449994.svg"
+  },
+  {
+    "revision": "549a9c57734ecec0801b701c0183ce70",
+    "url": "/react-crypto-monitor/static/media/Monero.549a9c57.svg"
+  },
+  {
+    "revision": "5eeb4b449a74b7f2404839b4a671dc75",
+    "url": "/react-crypto-monitor/static/media/NEM.5eeb4b44.svg"
+  },
+  {
+    "revision": "a01087f396dcd2fd163e473572e31964",
+    "url": "/react-crypto-monitor/static/media/NEO.a01087f3.svg"
+  },
+  {
+    "revision": "9fade3992aa70bb586b74fc3f4b7c423",
+    "url": "/react-crypto-monitor/static/media/Ontology.9fade399.svg"
+  },
+  {
+    "revision": "d8338822a9d5c5ab246557e2a934d23d",
+    "url": "/react-crypto-monitor/static/media/Ripple.d8338822.svg"
+  },
+  {
+    "revision": "2cf84eeda99f1d0dd0299c99df7e453f",
+    "url": "/react-crypto-monitor/static/media/Stellar.2cf84eed.svg"
+  },
+  {
+    "revision": "5e667fa83cd94f75f0c4b21c42dde15a",
+    "url": "/react-crypto-monitor/static/media/TRON.5e667fa8.svg"
+  },
+  {
+    "revision": "ba6036c1da13f53c38b52a4469bf37af",
+    "url": "/react-crypto-monitor/static/media/Tether.ba6036c1.svg"
+  }
+]);
